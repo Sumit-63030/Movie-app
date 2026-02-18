54465a0d775b86c9caff0a7d002c5bd53547e71c
Sumit-63030 <sumit.dharmadhikari05@gmail.com>
2026-02-19 01:11:59 +0530

    created the footer component

--- Movie_App/src/App.tsx
+++ Movie_App/src/App.tsx
@@ -1,6 +1,7 @@
 
 // import { useEffect, useState } from 'react'
 import './App.css'
+import Footer from './components/Footer'
 import Header from './components/Header'
 // import axios from 'axios';
 
@@ -10,7 +11,8 @@ function App() {
 
   return (
     <>
-    <Header/>
+    <Header />
+    <Footer />
     </>
   )
 }
---
+++ Movie_App/src/components/Footer.tsx
@@ -0,0 +1,26 @@
+import { HiFilm, HiFire, HiSearch } from 'react-icons/hi'
+import './Footer.css'
+import { HiTv } from 'react-icons/hi2'
+
+const Footer = () => {
+  const data = [
+    {icon : <HiFire /> , name : "Trending"},
+    {icon : <HiFilm /> , name : "Movies"},
+    {icon : <HiTv /> , name : "TV Series"},
+    {icon : <HiSearch /> , name : "Search"}
+  ]
+  return (
+    <div className='footer-container'>
+     {data.map((item , index) => (
+      <button key={index} className='footer-btn'>
+        <span className='footer-icon'>{item.icon}</span>
+        <span className='footer-icon'>{item.name}</span>
+      </button>
+     ))}
+      
+
+    </div>
+  )
+}
+
+export default Footer
