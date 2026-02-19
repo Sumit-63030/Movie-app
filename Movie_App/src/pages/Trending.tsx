fd95cff8a284e6fd417e17448392e0fae092e6e8
Sumit-63030 <sumit.dharmadhikari05@gmail.com>
2026-02-19 21:59:49 +0530

    Created the Trending Page

--- Movie_App/src/pages/Trending.tsx
+++ Movie_App/src/pages/Trending.tsx
@@ -1,11 +1,57 @@
+import { useEffect, useState } from 'react';
 import './Trending.css';
+import axios from 'axios';
+
+interface MovieItem {
+  id: number;
+  title?: string;
+  name?: string;
+  poster_path: string;
+  media_type: string;
+  release_date?: string;
+  first_air_date?: string;
+}
 
 const Trending = () => {
+  const [state, setState] = useState<MovieItem[]>([]);
+
+  
+
+  useEffect(() => {
+    const fetchTrending = async () => {
+    try {
+      const response = await axios.get(`https://api.themoviedb.org/3/trending/all/day?api_key=${import.meta.env.VITE_TMDB_API_KEY}`);
+
+      console.log(response.data.results);
+      setState(response.data.results);
+    } catch (error) {
+      console.log("Error fetching data", error)
+    }
+
+  } 
+    fetchTrending();
+  }, [])
+
   return (
     <div className="trending-container">
-      <p>trending</p>
+      <h2 className='page-title'>Trending Today</h2>
+      <div className="trending-grid">
+        {state && state.map((item: MovieItem) => (
+          <div className="movie-card" key={item.id}>
+            <img src={`https://image.tmdb.org/t/p/w500/${item.poster_path}`} alt="" className='poster' />
+            <b className='title'>{item.title || item.name}</b>
+            <div className="subtitle">
+              <span>{item.media_type == "tv" ? "TV Series" : "Movie"}</span>
+              <span>{item.release_date || item.first_air_date}</span>
+            </div>
+          </div>
+        ))}
+
+
+      </div>
     </div>
   )
 }
 
 export default Trending
+
