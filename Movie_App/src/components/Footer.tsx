d3799430912e059b49e1af02134c101bf49030e7
Sumit-63030 <sumit.dharmadhikari05@gmail.com>
2026-02-19 19:28:35 +0530

    used NavLink in footer component

--- Movie_App/src/components/Footer.tsx
+++ Movie_App/src/components/Footer.tsx
@@ -1,29 +1,32 @@
 import { HiFilm, HiFire, HiSearch } from 'react-icons/hi'
 import './Footer.css'
 import { HiTv } from 'react-icons/hi2'
+import { NavLink } from 'react-router-dom'
 
 const Footer = () => {
   const data = [
-    {icon : <HiFire /> , name : "Trending"},
-    {icon : <HiFilm /> , name : "Movies"},
-    {icon : <HiTv /> , name : "TV Series"},
-    {icon : <HiSearch /> , name : "Search"}
+    { icon: <HiFire />, name: "Trending", link: "/" },
+    { icon: <HiFilm />, name: "Movies", link: "/movies" },
+    { icon: <HiTv />, name: "TV Series", link: "/tv" },
+    { icon: <HiSearch />, name: "Search", link: "/search" }
   ]
   return (
     <div className='footer-container'>
 
-    <div className="footer-links">
-      {data.map((item , index) => (
-        <button key={index} className='footer-btn'>
-          <span className='footer-icon'>{item.icon}</span>
-          <span className='footer-icon'>{item.name}</span>
-        </button>
-      ))}
-    </div>
-    <div className="footer-text">Made by : Sumit Dharmadhikari</div>
+      <div className="footer-links">
+        {data.map((item, index) => (
+          <NavLink to={item.link}
+            key={index}
+            className= {({isActive}) => (isActive ? 'footer-btn active' : 'footer-btn')}>
+            <span className='footer-icon'>{item.icon}</span>
+            <span className='footer-icon'>{item.name}</span>
+          </NavLink>
+        ))} 
+      </div>
+      <div className="footer-text">Made by : Sumit Dharmadhikari</div>
 
     </div>
-    
+
   )
 }
 
