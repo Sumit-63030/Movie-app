03ab20a8157583a4f73be372f6ee718710ae9aac
Sumit-63030 <sumit.dharmadhikari05@gmail.com>
2026-03-24 10:04:22 +0530

    Created the Error page

--- Movie_App/src/pages/Error.tsx
+++ Movie_App/src/pages/Error.tsx
@@ -1,10 +1,27 @@
+import { useNavigate } from 'react-router-dom';
+import './Error.css';
 
 const Error = () => {
-  return (
-    <div>
-      Error
-    </div>
-  )
-}
+  const navigate = useNavigate();
 
-export default Error
+  return (
+    <div className="error-container">
+      <h1 className="error-code">404</h1>
+
+      <h2 className="error-text">Page Not Found</h2>
+
+      <p className="error-desc">
+        The page you are looking for does not exist.
+      </p>
+
+      <button 
+        className="home-button"
+        onClick={() => navigate('/')}
+      >
+        Go Home
+      </button>
+    </div>
+  );
+};
+
+export default Error;
