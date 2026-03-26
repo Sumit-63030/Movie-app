473ed35b97c03f2740f2c00cc882315382b4a393
Sumit-63030 <sumit.dharmadhikari05@gmail.com>
2026-03-26 14:46:34 +0530

    fix project structure and update search and details page responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 
 // import { useEffect, useState } from 'react'
-import './App.css'
+import './index.css';
 import Footer from './components/Footer'
 import Header from './components/Header'
 // import axios from 'axios';
